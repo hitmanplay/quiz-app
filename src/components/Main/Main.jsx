905ce2c0b22ff2a559e9e96574a49078622a560d
hitmanplay <hitman23416@gmail.com>
2024-06-06 15:10:14 +0300

    init

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './Header.module.css';
 import StartBtn from "./StartBtn/StartBtn";
+import Module from "./Module/Module";
 
 const Header = () => {
+    const [modal, setModal] = useState(false);
+
     return (
         <div className={classes.header}>
             <div className={classes.header__wrapper}>
-                <p className={classes.header__title}>Quizz Jambo</p>
+                <p className={classes.header__title}>Quizz App</p>
                 <p className={classes.header__subtitle}>Test Yourself</p>
-                <StartBtn />
+                <button className={classes.header__start} onClick={() => setModal(true)}>Start</button>
+                <Module visible={modal} setVisible={setModal}>123</Module>
             </div>
             <img src="'./../../../public/1613217901_94-p-fon-sinyaya-kletka-113.jpg'" alt="" className={classes.header__img}/>
         </div>
